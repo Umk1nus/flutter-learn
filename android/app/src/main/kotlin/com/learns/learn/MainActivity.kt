38f78912a46d16d38bcb64ef63fa4c5ef2a45df5
Umk1nus <umk1namw@gmail.com>
2024-11-17 23:18:24 +0300

    learn flutter

---
+++ android/app/src/main/kotlin/com/learns/learn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.learns.learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AppState();
+  }
+}
+
+class _AppState extends State<App> {
+  bool _loading = false;
+  double _progressValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _updateProgress() {
+    const sec = const Duration(milliseconds: 100);
+    Timer.periodic(sec, (Timer t) {
+      setState(() {
+        _progressValue += 0.01;
+
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text("App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+              decoration: _loading
+                  ? BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bg.png'),
+                          fit: BoxFit.fitHeight))
+                  : null,
+              padding: EdgeInsets.all(16),
+              child: _loading
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        LinearProgressIndicator(value: _progressValue),
+                        Text('${(_progressValue * 100).round()}%',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
+                      ],
+                    )
+                  : Text("Press button",
+                      style: TextStyle(color: Colors.white, fontSize: 20))),
+        ),
+        floatingActionButton: FloatingActionButton(
+            foregroundColor: Colors.pink,
+            onPressed: () => setState(() {
+                  _loading = !_loading;
+                  _updateProgress();
+                }),
+            child: Icon(Icons.cloud_download)),
+      ),
+    );
+  }
+}
